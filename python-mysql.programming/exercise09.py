fda4b3773e433181a3eb1fcdc4987a7ab41dd5c3
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-25 09:07:53 +0300

    Update exercise09.py

--- python-mysql.programming/exercise09.py
+++ python-mysql.programming/exercise09.py
@@ -6,11 +6,18 @@ my_connection = connector.connect(
     password="Secret_123",
     database="world"
 )
+
 my_cursor = my_connection.cursor()
+
+"""
+list countries with NO capital
+"""
 my_cursor.execute("""
   select co.code,co.name,co.continent 
   from country co
   where co.capital is null
 """)
+
+print("{:4} {:48} {:16}".format("Code", "Name", "Continent"))
 for row in my_cursor:
-    print(f"{row[0]}\t{row[1]}\t{row[2]}")
+    print(f"{row[0]:<4} {row[1]:48} {row[2]:16}")
