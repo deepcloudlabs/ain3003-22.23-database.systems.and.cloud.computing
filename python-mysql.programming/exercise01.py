d2046b4d7d71f5f0c0c159da422cc03b48d42985
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-25 08:45:33 +0300

    Update exercise01.py

--- python-mysql.programming/exercise01.py
+++ python-mysql.programming/exercise01.py
@@ -1,12 +1,69 @@
 from mysql import connector
 
+user = 'root'
+host = 'localhost'
+password = 'Secret_123'
+database = 'world'
+# create a connection
 my_connection = connector.connect(
-    host="localhost",
-    user="root",
-    password="Secret_123",
-    database="world"
+    host=host,
+    user=user,
+    password=password,
+    database=database
 )
 my_cursor = my_connection.cursor()
+
+"""
+mysql> help show
+Name: 'SHOW'
+Description:
+SHOW has many forms that provide information about databases, tables, columns, or status information about the server. 
+This section describes those following:
+
+SHOW {BINARY | MASTER} LOGS
+SHOW BINLOG EVENTS [IN 'log_name'] [FROM pos] [LIMIT [offset,] row_count]
+SHOW {CHARACTER SET | CHARSET} [like_or_where]
+SHOW COLLATION [like_or_where]
+SHOW [FULL] COLUMNS FROM tbl_name [FROM db_name] [like_or_where]
+SHOW CREATE DATABASE db_name
+SHOW CREATE EVENT event_name
+SHOW CREATE FUNCTION func_name
+SHOW CREATE PROCEDURE proc_name
+SHOW CREATE TABLE tbl_name
+SHOW CREATE TRIGGER trigger_name
+SHOW CREATE VIEW view_name
+SHOW DATABASES [like_or_where]
+SHOW ENGINE engine_name {STATUS | MUTEX}
+SHOW [STORAGE] ENGINES
+SHOW ERRORS [LIMIT [offset,] row_count]
+SHOW EVENTS
+SHOW FUNCTION CODE func_name
+SHOW FUNCTION STATUS [like_or_where]
+SHOW GRANTS FOR user
+SHOW INDEX FROM tbl_name [FROM db_name]
+SHOW MASTER STATUS
+SHOW OPEN TABLES [FROM db_name] [like_or_where]
+SHOW PLUGINS
+SHOW PROCEDURE CODE proc_name
+SHOW PROCEDURE STATUS [like_or_where]
+SHOW PRIVILEGES
+SHOW [FULL] PROCESSLIST
+SHOW PROFILE [types] [FOR QUERY n] [OFFSET n] [LIMIT n]
+SHOW PROFILES
+SHOW RELAYLOG EVENTS [IN 'log_name'] [FROM pos] [LIMIT [offset,] row_count]
+SHOW {REPLICAS | SLAVE HOSTS}
+SHOW {REPLICA | SLAVE} STATUS [FOR CHANNEL channel]
+SHOW [GLOBAL | SESSION] STATUS [like_or_where]
+SHOW TABLE STATUS [FROM db_name] [like_or_where]
+SHOW [FULL] TABLES [FROM db_name] [like_or_where]
+SHOW TRIGGERS [FROM db_name] [like_or_where]
+SHOW [GLOBAL | SESSION] VARIABLES [like_or_where]
+SHOW WARNINGS [LIMIT [offset,] row_count]
+"""
 my_cursor.execute("show tables")
+for table_name in my_cursor:
+    print(table_name[0])
+
+my_cursor.execute("show collation")
 for table_name in my_cursor:
     print(table_name[0])
