6b031ab5ee1950e445bd3a408e8b19afc8f88749
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-25 09:10:52 +0300

    Update exercise11.py

--- python-mysql.programming/exercise11.py
+++ python-mysql.programming/exercise11.py
@@ -6,12 +6,19 @@ my_connection = connector.connect(
     password="Secret_123",
     database="world"
 )
+
 my_cursor = my_connection.cursor()
+
+"""
+List the top 10 largest country with respect to surface area 
+"""
 my_cursor.execute("""
   select name,surfaceArea 
   from country 
   order by surfaceArea desc
   limit 10
 """)
+
+print("{:40} {:8}".format("Name", "Surface Area"))
 for row in my_cursor:
-    print(f"{row[0]}\t{row[1]}")
+    print(f"{row[0]:40} {row[1]:8}")
