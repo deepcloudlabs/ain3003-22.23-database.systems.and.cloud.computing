cbd7288e9dce4d216a7dbddb2f166e14d22c97f7
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-25 09:03:25 +0300

    Update exercise08.py

--- python-mysql.programming/exercise08.py
+++ python-mysql.programming/exercise08.py
@@ -6,11 +6,18 @@ my_connection = connector.connect(
     password="Secret_123",
     database="world"
 )
+
 my_cursor = my_connection.cursor()
+
+"""
+list Turkish speaking countries
+"""
 my_cursor.execute("""
-  select cl.countrycode,cl.language,c.name 
+  select cl.language,c.name 
   from countrylanguage cl,country c
   where cl.language='turkish' AND c.code=cl.countrycode
 """)
+
+print("{:12} {:40}".format("Language", "Country Name"))
 for row in my_cursor:
-    print(f"{row[0]}\t{row[1]}\t{row[2]}")
+    print(f"{row[0]:12} {row[1]:40}")
