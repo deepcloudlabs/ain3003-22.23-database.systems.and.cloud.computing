ef368bf2384d42796625ff9af54a735e403e6f3b
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-25 08:57:21 +0300

    add comments and formatting

--- python-mysql.programming/exercise05.py
+++ python-mysql.programming/exercise05.py
@@ -6,7 +6,12 @@ my_connection = connector.connect(
     password="Secret_123",
     database="world"
 )
+
 my_cursor = my_connection.cursor()
+
+"""
+list all countries with population > 50000000 in europe or africa
+"""
 my_cursor.execute("""
   select code,name,continent,population 
   from country
@@ -14,5 +19,6 @@ my_cursor.execute("""
   AND Continent in ( 'Europe' , 'Africa' )
   limit 10
 """)
+
 for row in my_cursor:
     print(f"{row[1]}\t{row[2]}\t{row[3]}")
--- python-mysql.programming/exercise06.py
+++ python-mysql.programming/exercise06.py
@@ -6,11 +6,19 @@ my_connection = connector.connect(
     password="Secret_123",
     database="world"
 )
+
 my_cursor = my_connection.cursor()
+
+"""
+list population of the following cities in Turkey: Istanbul , Ankara, and Izmir
+"""
 my_cursor.execute("""
   select id,name,population 
   from city
-  where name in ( 'Istanbul' , 'Ankara', 'Izmir' )
+  where name in ( 'Istanbul' , 'Ankara', 'Izmir' ) 
+  AND countrycode='TUR'
 """)
+
+print("{:4}\t{:12}\t{:10}".format("ID", "Name", "Population"))
 for row in my_cursor:
-    print(f"{row[0]}\t{row[1]}\t{row[2]}")
+    print(f"{row[0]:4}\t{row[1]:12}\t{row[2]:<10}")
