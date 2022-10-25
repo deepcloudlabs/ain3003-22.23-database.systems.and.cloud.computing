a2c6a21235f08cca8513a12d8ee849b0c10b40e4
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-25 09:00:57 +0300

    Update exercise07.py

--- python-mysql.programming/exercise07.py
+++ python-mysql.programming/exercise07.py
@@ -6,11 +6,18 @@ my_connection = connector.connect(
     password="Secret_123",
     database="world"
 )
+
 my_cursor = my_connection.cursor()
+
+"""
+sort continents by continent in descending order, then by name in ascending order
+"""
 my_cursor.execute("""
   select name,continent 
   from country
   order by continent desc, name asc
 """)
+
+print("{:40} {:12}".format("Name", "Continent"))
 for row in my_cursor:
-    print(f"{row[0]}\t{row[1]}")
+    print(f"{row[0]:40} {row[1]:12}")
