10d6f8fed313184c5e11109f1863778bf2f43699
Binnur KURT <deepcloudlabs@gmail.com>
2022-10-29 09:12:06 +0300

    Update exercise15.py

--- python-mysql.programming/exercise15.py
+++ python-mysql.programming/exercise15.py
@@ -15,10 +15,11 @@ Sort the countries by number of their cities in descending order
 my_cursor.execute("set session sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))")
 
 my_cursor.execute("""
-    select co.name, count(ci.name)
+    select co.name, count(ci.name) as noc
     from country co, city ci
     where ci.countrycode = co.code
     group by co.code
+    order by noc desc
 """)
 
 print("{:40} {:3}".format("Country Name", "Number of  Cities"))
